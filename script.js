e93ae50a84faf26bb5352ca6f37a757350f937b1
sgrdev <95933073+sgrdev@users.noreply.github.com>
2024-08-14 22:12:11 +0545

    routing-and-highlight-active-links

---
+++ script.js
@@ -0,0 +1,35 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+//hightlight active link
+function highLightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+//INIT app
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('tv Details');
+      break;
+  }
+  highLightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
