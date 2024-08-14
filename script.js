da38fb1dcd44c0b3bce915d15d0120a5d958d6a8
sgrdev <95933073+sgrdev@users.noreply.github.com>
2024-08-14 23:14:02 +0545

    intrigated-api-popular-movies-section-fetched-and-displayed

--- script.js
+++ script.js
@@ -2,6 +2,48 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData('movie/popular');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    
+            <a href="movie-details.html?id=${movie.id}">
+              ${
+                movie.poster_path
+                  ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />`
+                  : `<img src="img/no-image.jpg" alt="${movie.title}" />`
+              }
+              <div class="card-body">
+                <h5 class="card-title">${movie.title}</h5>
+                <p class="card-text">
+                  <small class="text-muted">Release: ${
+                    movie.release_date
+                  }</small>
+                </p>
+              </div>
+            </a>
+          `;
+
+    document.querySelector('#popular-movies').appendChild(div);
+  });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '3e34206224fcf40f2db33b7ee7ed9e2b';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+
+  const data = await response.json();
+  return data;
+}
+
 //hightlight active link
 function highLightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -17,7 +59,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case '/index.html':
-      console.log('Home');
+      displayPopularMovies();
       break;
     case '/shows.html':
       console.log('Shows');
